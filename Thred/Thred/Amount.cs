a29b872bd554ad179055563688007b991f340c7e
AMEYAULEMALE <75296688+AMEYAULEMALE@users.noreply.github.com>
2020-12-01 12:21:51 +0530

    Add files via upload

---
+++ Thred/Thred/Amount.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Thred
+{
+    class Amount
+    {
+        double NetBalance = 50000;
+        double transaction;
+
+
+
+
+        public void deposit()
+        {
+            NetBalance = NetBalance + transaction;
+            Console.WriteLine(NetBalance);
+        }
+
+
+
+
+        public void withdrawal()
+        {
+            NetBalance = NetBalance - transaction;
+            Console.WriteLine(NetBalance);
+        }
+
+
+
+
+        public  Amount(double transaction)
+        {
+            this.transaction = transaction;
+        }
+        public double Netbalance
+        {
+            get
+            {
+                return NetBalance;
+            }
+            set
+            {
+                NetBalance = value;
+            }
+        }
+
+
+    }
+}
